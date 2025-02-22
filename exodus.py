0092b8ebaac0e5834387fa24d64a4b48f7ef6057
deadend1415 <ahipuedaniels@gmail.com>
2025-02-22 22:21:35 +0100

    Organized the repo

---
+++ exodus.py
@@ -0,0 +1,139 @@
+#########################
+# Esercizio sulle tuple
+global_collecion = tuple(("mela","banana","kiwi"))
+global_dictionary = {
+        "nome" : "Mario",
+        "cognome" : "Rossi",
+        "età" : 30
+    }
+def Esercizio1():
+    print("-----------------------------")
+    collection = tuple(())
+    print(type(collection))
+# end def
+def Esercizio2():
+    print("-----------------------------")
+    collecion = tuple(("mela","banana","kiwi"))
+    print(collecion)
+# end def
+def Esercizio3():
+    print("-----------------------------")
+    print(global_collecion[1])
+# end def
+def Esercizio4():
+    print("-----------------------------")
+    new = global_collecion[0:2]
+    print(new)
+# end def
+def Esercizio5():
+    print("-----------------------------")
+    if (global_collecion.count("mela") > 0):
+        # comment: 
+        print("Trovato!")
+    else:
+        # comment: 
+        print("Assente")
+    # end if
+# end def
+def Esercizio6():
+    print("-----------------------------")
+    print(len(global_collecion))
+# end def
+def Esercizio7():
+    print("-----------------------------")
+    tupla2 = ("pesca" , "arancia")
+    new = global_collecion + tupla2
+    print(new)
+# end def
+#########################
+# Esercizi su set
+def Esercizio8():
+    print('-----------------------------')
+    new = set(())
+    print(type(new))
+# end def
+def Esercizio9():
+    print('-----------------------------')
+    new = {"mela","banana","kiwi","mela"}
+    print(new)
+# end def
+#########################
+# Esercizi Dictionary
+def Esercizio10():
+    print('-----------------------------')
+    new = {}
+    print(type(new))
+# end def
+def Esercizio11():
+    new = {
+        "nome" : "Mario",
+        "cognome" : "Rossi",
+        "età" : 30
+    }
+    print(new)
+# end def
+def Esercizioni12():
+    print(global_dictionary["età"])
+# end def
+def Esercizioni13():
+    dictionary = global_dictionary
+    dictionary["email"] = "mario.rossi@email.com"
+    print(global_dictionary)
+# end def
+def Esercizioni14():
+    dictionary = global_dictionary.keys()
+    print(dictionary)
+# end def
+
+######################### MAIN #############################
+while (global_collecion != 0):
+    # comment: 
+    option = input("Please enter: ")
+    match (option):
+        case ('1'):
+            # comment:
+            Esercizio1() 
+        case ('2'):
+            # comment: 
+            Esercizio2()
+        case ('3'):
+            # comment: 
+            Esercizio3()
+        case ('4'):
+            # comment: 
+            Esercizio4()
+        case ('5'):
+            # comment: 
+            Esercizio5()
+        case ('6'):
+            # comment: 
+            Esercizio6()
+        case ('7'):
+            # comment: 
+            Esercizio7()
+        case ('8'):
+            # comment: 
+            Esercizio8()
+        case ('9'):
+            # comment: 
+            Esercizio9()
+        case ('10'):
+            # comment: 
+            Esercizio10()
+        case ('11'):
+            # comment: 
+            Esercizio11()
+    # end match 
+        case ('12'):
+            # comment: 
+            Esercizio12()
+    # end match 
+        case ('13'):
+            # comment: 
+            Esercizio13()
+    # end match 
+        case ('14'):
+            # comment: 
+            Esercizio14()
+    # end match 
+# end while
---
+++ genesis.py
@@ -0,0 +1,78 @@
+"""Hi John
+You are Gay 
+but why 
+John Xi Gay"""
+print("--------------------------------------------------------------------------")
+#########################
+# Esercizio sulle variabli
+def Esercizio1():
+    print("Esercizio sulle variabili" + "\n-----------------------------")
+    nome = input("Input name ")
+    print(nome)
+#########################
+# Esercizio sulle stringhe
+def Esercizio2():
+    print("Esercizio sulle stringhe" + "\n-----------------------------")
+    stringa =  "Hello World!"
+    print(stringa.upper())
+# end def
+def Esercizio3():
+    print("\n-----------------------------")
+    stringa = "il meglio deve ancora venire"
+    print(stringa.split())
+# end def
+def Esercizio4():
+    print("\n-----------------------------")
+    stringa = " Ciao "
+    print(stringa.strip())
+# end def
+#########################
+# Esercizio sulle operazioni matematiche
+def Esercizio5():
+    print("Esercizio sulle operazioni matematiche " + "\n-----------------------------")
+    numero = 4
+    numero1 = 1
+    numero2 = numero - numero1
+    print(numero2)
+# end def    
+def Esercizio6():
+    print("\n-----------------------------")
+    numero = 4
+    numero1 = 1
+    numero2 = numero + numero1
+    print(numero2)
+#########################
+# Esercizio su tipi di dato
+def Esercizio7():
+    print("Esercizio su tipi di dato " + "\n-----------------------------")
+    numero = 4
+    print(type(numero))
+# end def    
+def Esercizio8():
+    print("Esercizio su tipi di dato " + "\n-----------------------------")
+    numero_decimale = "float"
+    print(type(numero_decimale))
+#########################
+# Esercizio condizionali
+def Esercizio9():
+    print("Esercizio condizionali " + "\n-----------------------------")
+    x = int(input("Please enter: "))
+    if (x > 0): 
+        print("Il numero è positivo")
+    else: 
+        print("Il numero è negativo ")
+    # end if
+# end def
+def Esercizio10():
+    if (x > y):
+        # comment:
+        print("Il numero è maggiore") 
+    elif (x == y):
+        # comment: 
+        print("I numeri sonon uguali")
+    else:
+        # comment: 
+        print("Il secondo numero è maggiore")
+    # end if
+    
+# end def
